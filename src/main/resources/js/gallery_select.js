41d4c7c4cb3d879fc74ca5f2d5d2aa678b1cf904
ChaseTheSkunk <chasereynard@gmail.com>
2015-08-24 20:50:56 -0400

    Added first gallery page

---
+++ src/main/resources/js/gallery_select.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Chase the Skunk on 8/23/2015.
+ */
+
+$(document).ready(function() {
+    var characterSidebar = new Sidebar([
+        {
+            type: "link",
+            href: "chase.html",
+            text: "Normal"
+        },
+        {
+            type: "link",
+            href: "transformation.html",
+            text: "Transformation"
+        },
+        {
+            type: "link",
+            href: "toony.html",
+            text: "Toony Antics"
+        }
+    ]);
+
+    characterSidebar.matchSize("content");
+    characterSidebar.arrangeLinks();
+});
