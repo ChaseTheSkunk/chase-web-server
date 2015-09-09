0467d50bb75a3c6ddcd99a4c03a769ae8aa4d5c0
ChaseTheSkunk <chasereynard@gmail.com>
2015-09-09 17:56:44 -0400

    Added sidebar for world intro pages

---
+++ src/main/resources/js/world_intro_select.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Chase the Skunk on 9/9/2015.
+ */
+
+$(document).ready(function() {
+    var characterSidebar = new Sidebar([
+        {
+            type: "link",
+            href: "index.html",
+            text: "World Home"
+        },
+        {
+            type: "link",
+            href: "geography.html",
+            text: "Geography"
+        },
+        {
+            type: "link",
+            href: "history.html",
+            text: "History"
+        },
+        {
+            type: "link",
+            href: "magic.html",
+            text: "Magic"
+        },
+        {
+            type: "link",
+            href: "pantheon.html",
+            text: "Pantheon"
+        },
+        {
+            type: "link",
+            href: "races.html",
+            text: "Races"
+        }
+    ]);
+
+    characterSidebar.matchContainerSize();
+    characterSidebar.arrangeLinks();
+});
