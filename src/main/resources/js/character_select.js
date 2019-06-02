1fee6ce25efa26e1b4b6abfa1993874f8e1d5a9c
ChaseTheFox <chasereynard@gmail.com>
2019-06-02 12:31:03 -0400

    Added character page for Azalea

--- src/main/resources/js/character_select.js
+++ src/main/resources/js/character_select.js
@@ -73,6 +73,11 @@ $(document).ready(function() {
             type: "link",
             href: "foxblade.html",
             text: "Foxblade"
+        },
+        {
+            type: "link",
+            href: "azalea.html",
+            text: "Azalea"
         }
     ]);
 
